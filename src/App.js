7f5aa34b5abc0abc824b8c5a255d22fb5663aeb6
limhyeonggeun <lhg010115@gmail.com>
2025-11-02 01:02:57 +0900

    Update banner video, poster, and style fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Banner from './components/Banner';
 import Concept from './components/Concept';
+import Poster from './components/Poster';
 import './styles/global.css';
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       <Header />
       <Banner />
       <Concept />
+      <Poster />
     </>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,13 @@
 import '../styles/Banner.css';
 import CaretDown from '../assets/svg/CaretDown.svg';
+import BannerVideo from '../assets/videos/main_bg.mp4'
 
 export default function Banner() {
   return (
     <section className="Banner">
       <video
         className="banner-video"
-        src="/assets/videos/banner.mp4"
+        src={BannerVideo}
         autoPlay
         loop
         muted
@@ -14,7 +15,7 @@ export default function Banner() {
       />
       <div className="banner-overlay">
         <div>
-          <h1 className="title">RE :</h1>
+          <h1 className="title">RE:</h1>
           <p className="subtitle-eng">2025 SWU MEDIA CONTENTS EXHIBITION</p>
           <p className="subtitle-kr">서원대학교 미디어콘텐츠학과 졸업전시회</p>
         </div>
