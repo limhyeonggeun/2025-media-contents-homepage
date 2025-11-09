39972242b6766bd65488fe2764026340cdec3afa
임형근 <imhyeong-geun@imhyeong-geun-ui-MacBookAir.local>
2025-11-09 16:24:33 +0900

    홈페이지 올린 버전

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Project from "./pages/Project";
@@ -10,17 +10,17 @@ import ScrollTopButton from "./components/ScrollTopButton";
 
 export default function App() {
   return (
-    <Router>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project" element={<Project />} />
-        <Route path="/department" element={<DepartmentInfo/>} />
-        <Route path="/gallery" element={<Gallery />} /> 
+        <Route path="/department" element={<DepartmentInfo />} />
+        <Route path="/gallery" element={<Gallery />} />
         <Route path="/guestbook" element={<GuestBook />} />
       </Routes>
       <Footer />
       <ScrollTopButton />
-    </Router>
+    </>
   );
 }
